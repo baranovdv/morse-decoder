95b070453aa44fb4d47197c27d5eaf162b4b29fa
baranovdv <baranov.d.v.1988@gmail.com>
2023-02-10 15:20:43 +0700

    -feat: md task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,35 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = ''
+    for(let i = 0; i < expr.length; i = i + 10) {
+        // console.log(expr.slice(i,i+10))
+        let r = ''
+        let buf = expr.slice(i,i+10)
+
+        if(buf == '**********') {
+            result = result + ' '
+            continue
+        }
+        for(let j = 0; j <= 4; j++){
+            if(buf.slice(buf.length - 2*j -2,buf.length - 2*j) == '10') {
+                r = '.' + r
+            }
+            if(buf.slice(buf.length - 2*j -2,buf.length - 2*j) == '11') {
+                r = '-' + r
+            }
+            // console.log(r)
+        }
+        // console.log(r)
+        result += MORSE_TABLE[r]
+
+    }
+// console.log(result)
+return result
 }
 
 module.exports = {
     decode
 }
+
+// console.log(module.exports.decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
